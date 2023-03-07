3be22d1520ee437cbfe6b7a4b57c53b02536655c
Amos Lee <iwillhappy1314@gmail.com>
2023-03-07 16:44:06 +0800

    slugify export file name

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ import tarfile
 import argparse
 from datetime import datetime
 from requests.exceptions import ConnectionError
+import re
+
+
+def slugify(text):
+    # 将所有非字母数字字符替换为 -
+    text = re.sub(r'[^a-zA-Z0-9]+', '-', text)
+    # 将多个 - 替换为一个 -
+    text = re.sub(r'-+', '-', text)
+    # 将字符串转换为小写
+    text = text.lower()
+    # 去除开头和结尾的 -
+    text = text.strip('-')
+    return text
 
 
 def main():
@@ -39,6 +52,7 @@ def main():
 
         for sd in site_data:
             url = f'http://127.0.0.1:{site_data[sd]["port"]}/'
+            export_file_name = slugify(site_data[sd]["name"])
 
             try:
                 requests.get(url)
@@ -49,15 +63,15 @@ def main():
                 os.environ["MYSQL_HOME"] = f'{local_path}/run/{sd}/conf/mysql'
                 os.environ["WP_CLI_CONFIG_PATH"] = os.path.join(local_bin_path, "wp-cli/config.yaml")
 
-                os.system(f'wp db export {site_data[sd]["name"]}.sql')
+                os.system(f'wp db export {export_file_name}.sql')
 
                 # 移动.sql文件到文件夹
-                source_file_path = os.path.join(os.getcwd(), f'{site_data[sd]["name"]}.sql')
-                target_file_path = os.path.join(folder_path, f'{site_data[sd]["name"]}.sql')
+                source_file_path = os.path.join(os.getcwd(), f'{export_file_name}.sql')
+                target_file_path = os.path.join(folder_path, f'{export_file_name}.sql')
                 shutil.move(source_file_path, target_file_path)
 
             except ConnectionError:
-                print(f'{site_data[sd]["name"]} not running, skip it.')
+                print(f'{export_file_name} not running, skip it.')
 
         # 压缩文件夹为.tar.gz文件
         output_file_path = os.path.join(backup_path, today + ".tar.gz")
